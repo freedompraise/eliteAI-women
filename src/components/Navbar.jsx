b4ceabdf09e4c3a73f337486c178b3ce24738fb4
Praise Freedom Dike <dikepraise119@gmail.com>
2025-01-16 22:43:14 +0100

    feat: enhance Navbar component with improved mobile menu functionality and accessibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,14 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
+      {/* Logo */}
       <img
         src={logo}
         alt="hoobank"
         className="w-[28px] h-[28px] object-contain"
       />
 
+      {/* Desktop Nav Links */}
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
@@ -32,6 +34,7 @@ const Navbar = () => {
         ))}
       </ul>
 
+      {/* Mobile Menu */}
       <div className="sm:hidden flex flex-1 justify-end items-center">
         <img
           src={toggle ? close : menu}
@@ -40,19 +43,23 @@ const Navbar = () => {
           onClick={() => setToggle(!toggle)}
         />
 
+        {/* Sidebar for Mobile */}
         <div
           className={`${
-            !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+            toggle ? "flex h-16" : "hidden"
+          } p-6 fixed top-16 left-16 w-screen z-[100]`}
         >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+          <ul className="list-none flex flex-col justify-center items-center flex-1">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                className={`font-poppins font-medium cursor-pointer text-[16px] mb-4 ${
                   active === nav.title ? "text-white" : "text-dimWhite"
-                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-                onClick={() => setActive(nav.title)}
+                }`}
+                onClick={() => {
+                  setActive(nav.title);
+                  setToggle(false); // Close menu on selection
+                }}
               >
                 <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
