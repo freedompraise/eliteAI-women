d11a9eeab3153681d10a83e2f0ae02ce203b6f28
Praise Freedom Dike <dikepraise119@gmail.com>
2025-01-16 16:22:23 +0100

    refactor: update logo format and enhance navbar responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,21 +9,25 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <img
+        src={logo}
+        alt="hoobank"
+        className="w-[28px] h-[28px] object-contain"
+      />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`group font-poppins cursor-pointer text-[16px] ${
-              active === nav.title ? "text-secondary font-semibold" : "text-dimWhite font-regular"
+              active === nav.title
+                ? "text-secondary font-semibold"
+                : "text-dimWhite font-regular"
             } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
             onClick={() => setActive(nav.title)}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
-            <div
-              class="h-0.5 bg-secondary scale-x-0 group-hover:scale-100 transition-transform origin-left rounded-full duration-300 ease-out"
-            />
+            <div class="h-0.5 bg-secondary scale-x-0 group-hover:scale-100 transition-transform origin-left rounded-full duration-300 ease-out" />
           </li>
         ))}
       </ul>
