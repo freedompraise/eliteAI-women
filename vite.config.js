1b940614f8e774391fdf7e3ec8312d828713166b
Praise Freedom Dike <dikepraise119@gmail.com>
2025-02-04 09:10:13 +0100

    feat: enhance Vite configuration with path aliasing and external rollup options

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// Example vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["#minpath"],
+    },
+  },
+});
